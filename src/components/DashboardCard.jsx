959a3d00ee5f2630db59d340bccf1c83d6469110
joyce-rocha <joycerocha.j@gmail.com>
2026-05-23 00:56:11 -0300

    feat: create reusable dashboard card

---
+++ src/components/DashboardCard.jsx
@@ -0,0 +1,11 @@
+// import info from "../info";
+function DashboardCard(info) {
+  return (
+    <div className="card">
+      <h3>{info.title}</h3>
+      <p>{info.value}</p>
+    </div>
+  );
+}
+
+export default DashboardCard;
---
+++ src/info.js
@@ -0,0 +1,24 @@
+let info = [
+  {
+    id: 1,
+    title: "Vehicles",
+    value: 12,
+  },
+  {
+    id: 2,
+    title: "Drivers",
+    value: 8,
+  },
+  {
+    id: 3,
+    title: "Missions",
+    value: 4,
+  },
+  {
+    id: 4,
+    title: "Alerts",
+    value: 3,
+  },
+];
+
+export default info;
